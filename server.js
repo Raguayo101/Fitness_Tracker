ac51ca1e595f1fe48dcfb491e6c40a39868e4700
Drigo <rodrigoaguayo94@gmail.com>
2021-09-02 22:51:24 -0700

    added new string to heroku

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
 const app = express();
-
-const PORT = 3000;
-
-
 
 app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:3000/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}
+);
 
 app.use(require('./routes/api'));
 app.use(require('./routes/view'));
