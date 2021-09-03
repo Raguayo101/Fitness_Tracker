3418ea68196653fca262d3bb154b619820b126ee
Drigo <rodrigoaguayo94@gmail.com>
2021-09-02 21:27:43 -0700

    creating models and fitness js

---
+++ models/fitness.js
@@ -0,0 +1,45 @@
+const mongoose = require('pmongoose');
+
+const Schema = mongoose.Schema;
+
+const fitnessSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new DAte(),
+    },
+    exervises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: 'Input an exercise type'
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: 'Input an exercise name'
+            },
+            duration: {
+                type: Number,
+                required: 'Input an exercise length in minuets'
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+
+        }
+    ]
+})
+
+const Fitness = mongoose.model('Get in some healthy exercises!', fitnessSchema);
+
+module.exports = Fitness;
