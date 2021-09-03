72448d148815135107d41883157abf7e3da810a4
Drigo <rodrigoaguayo94@gmail.com>
2021-09-02 21:40:20 -0700

    adding api.js and routes for our site

---
+++ routes/api.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const Fitness = require('../models/fitness.js');
+
+// route for workouts
+router.post('/api/workouts', (req, res) => {
+    Fitness.create({})
+        .then((dbFitness) => {
+            res.json(dbFitness);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// router for workout w/id
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    Fitness.findByIdandUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
+        .then((dbFitness) => {
+            res.json(dbFitness);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.get('/api/workouts', (req, res) => {
+    Fitness.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+        .then((dbFitness) => {
+            res.json(dbFitness);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    Fitness.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+        .sort({ _id: -1 })
+        .limit(7)
+        .then((dbFitness) => {
+            res.json(dbFitness);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.delete('/api/workouts', ({ body }, res) => {
+    Fitness.findByIdandDelete(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
